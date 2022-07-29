ee70838d1f18978df54602eea21c83e444917b17
Luis E Moreno <lemiliomoreno@gmail.com>
2022-07-28 20:41:03 -0500

    Add alembic

--- 8-python-fastapi-database/api/alembic/env.py
+++ 8-python-fastapi-database/api/alembic/env.py
@@ -7,6 +7,7 @@ from sqlalchemy import pool
 from alembic import context
 from models.database import Base
 
+
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
